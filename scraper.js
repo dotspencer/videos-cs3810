20359a4888dd5297d638da184b03c7c089d4a0b7
Spencer Smith <dontaskspencer@gmail.com>
2017-02-14 12:32:15 -0700

    Initial

---
+++ scraper.js
@@ -0,0 +1,33 @@
+var videos = [];
+
+for (var i = 0; i < rows.length; i++) {
+  var cells = rows[i].querySelectorAll('td');
+  if (cells.length == 0) {
+    continue;
+  }
+
+  var title = cells[1].innerText;
+  title = title.substring(0, title.length - 1);
+  var vid_links = cells[4].querySelectorAll('a');
+
+  if (vid_links.length == 0) {
+    continue;
+  }
+
+  var group = {
+    title: title,
+    videos: []
+  };
+
+  for (var j = 0; j < vid_links.length; j++) {
+    if (vid_links[j].innerText.indexOf('Note') != -1) {
+      continue;
+    }
+    var id = vid_links[j].href.split("/").pop();
+    group.videos.push(id);
+  }
+
+  videos.push(group);
+}
+
+JSON.stringify(videos, null, 2);
